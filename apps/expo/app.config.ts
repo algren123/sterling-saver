08f1d19654ac3ad3de47db26a12c50a586faa476
algren123 <algren-michael.pauna@skybettingandgaming.com>
2023-09-03 00:58:38 +0100

    Initial commit

---
+++ apps/expo/app.config.ts
@@ -0,0 +1,28 @@
+import { ConfigContext, ExpoConfig } from '@expo/config'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  extra: {
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID || 'f15cda53-fbc4-4f22-8cba-b00cb672a6aa',
+    },
+  },
+  owner: process.env.EAS_OWNER || 'algren123',
+  plugins: ['expo-router'],
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+  platforms: ['ios', 'android'],
+  name: 'sterling-silver',
+  slug: 'sterling-silver',
+  updates: {
+    url: 'https://u.expo.dev/85fc6ccd-0ce1-4e4d-804c-b15df989f97e',
+  },
+  runtimeVersion: {
+    policy: 'sdkVersion',
+  },
+})
